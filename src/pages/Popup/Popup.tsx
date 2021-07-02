b9f551a6b5061d03c38ec9faecfbc2c51cd4641f
Alvaro Vidal <alvaro.vidal@dynatrace.com>
2021-07-02 14:14:45 +0200

    Added tooltip deletion & status change capabilities

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -2,21 +2,49 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 import { ActionType, MessageType, Tool, ToolTypes } from '../../core/types';
-import { Button, Collapse, Switch } from 'antd';
+import { Button, Collapse, Switch, Tooltip } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
 import { Add } from '../Add/Add';
+
 const { Panel } = Collapse;
 
 const Popup: React.FC<{}> = () => {
-
   const Tool = (params: any) => {
     const tool: Tool = params.data;
+
+    const onDeleteHandler = () => {
+      chrome.runtime.sendMessage({
+        type: ActionType.DELETE_TOOL,
+        id: tool.id,
+      });
+    };
+
+    const onSwitchHandler = () => {
+      chrome.runtime.sendMessage({
+        type: ActionType.CHANGE_STATUS,
+        id: tool.id,
+        enabled: !tool.enabled,
+      });
+    };
+
     return (
       <div className="action">
-        <Switch checked={tool.enabled} />
-        <span>{tool.name}</span>
+        <div>
+          <Switch checked={tool.enabled} onClick={onSwitchHandler} />
+          <span>{tool.name}</span>
+        </div>
+        <Tooltip title="delete">
+          <Button
+            type="primary"
+            shape="circle"
+            danger
+            icon={<DeleteOutlined />}
+            onClick={onDeleteHandler}
+          />
+        </Tooltip>
       </div>
     );
-  }
+  };
 
   const [tools, setTools] = React.useState<Tool[]>([]);
 
@@ -43,11 +71,18 @@ const Popup: React.FC<{}> = () => {
     const popupOptions = {
       width: 400,
       height: 600,
-      left: (screen.width / 2) - (400 / 2),
-      top: (screen.height / 2) - (600 / 2),
+      left: screen.width / 2 - 400 / 2,
+      top: screen.height / 2 - 600 / 2,
     };
     onClear();
-    chrome.windows.create({ 'url': 'add.html', 'type': 'popup', width: popupOptions.width, height: popupOptions.height, left: popupOptions.left, top: popupOptions.top });
+    chrome.windows.create({
+      url: 'add.html',
+      type: 'popup',
+      width: popupOptions.width,
+      height: popupOptions.height,
+      left: popupOptions.left,
+      top: popupOptions.top,
+    });
     //chrome.runtime.sendMessage({ type: ActionType.ADD_TOOL, tool: { name: Math.random().toString(), enabled: true, value: "asdasdd", type: ToolTypes.PASTE } });
   };
 
@@ -57,14 +92,12 @@ const Popup: React.FC<{}> = () => {
         <Collapse defaultActiveKey={['tools']}>
           <Panel header="Current Tools" key="tools">
             <div className="action-list">
-              {
-                tools.map(
-                  tool => <Tool data={tool}></Tool>
-                )
-              }
-              {tools.length == 0 &&
+              {tools.map((tool) => (
+                <Tool data={tool}></Tool>
+              ))}
+              {tools.length == 0 && (
                 <p>There's no tools! Start by creating a new one</p>
-              }
+              )}
             </div>
           </Panel>
           <Panel header="Add new tool" key="add">
